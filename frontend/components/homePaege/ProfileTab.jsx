6615df7e18c7fe0f86c2ba40e1b1bf6f8ee66518
ruchik-panwal <ruchik.panwal@gmail.com>
2026-06-22 14:03:28 +0000

    added react-icons and also completed ProfileTab

--- frontend/components/homePaege/ProfileTab.jsx
+++ frontend/components/homePaege/ProfileTab.jsx
@@ -1,14 +1,19 @@
 import Image from "next/image";
+import { IoIosSettings } from "react-icons/io";
 
 export default function ProfileTab() {
   return (
-    <div className="w-full h-full bg-accent">
+    <div className="bg-accent flex flex-col w-full gap-2 p-2">
       <Image
         alt="Profile Image"
         src="/AkiChSaki.jpg"
         width={500}
         height={500}
       />
+      <button className="text-background flex items-center text-[1.3rem] gap-1">
+        <IoIosSettings className="text-[1.7rem]" />
+        Settings
+      </button>
     </div>
   );
 }
