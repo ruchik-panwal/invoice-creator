d59e8013d0815e15e2c2038d91c39f6fe77164b1
ruchik-panwal <ruchik.panwal@gmail.com>
2026-06-22 12:45:55 +0000

    header component

---
+++ frontend/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <div className="w-full flex justify-between overflow-hidden">
+            <h1 className="font-koulen text-foreground tracking-[-5%] leading-[0.8] text-[clamp(3rem,20vw,6rem)]">
+                Pinvoice
+            </h1>
+        </div>
+    )
+}
