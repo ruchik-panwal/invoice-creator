4d1473e0a3ece56a25d2fd462e97cb51111c8e33
ruchik-panwal <ruchik.panwal@gmail.com>
2026-06-24 19:28:27 +0000

    funck typescript

--- frontend/app/invoice/page.tsx
+++ frontend/app/invoice/page.tsx
@@ -1,7 +1,67 @@
+import ListForms from "@/components/InvPage/ListForms";
+import { IoIosArrowDropdown } from "react-icons/io";
+
 export default function NewInvoice() {
   return (
-    <div className="bg-sky-200 h-full w-full flex">
-     
+    <div className="uppercase h-full w-full flex text-background ">
+      <div className="h-full w-full flex items-center border-r-3 border-accent">
+        <div className="flex flex-col gap-4 h-full w-full p-4">
+          <DateForm />
+          <FromForm />
+          <ToForm />
+          <MiscForm />
+        </div>
+
+        <ListForms />
+      </div>
+
+      <div className="flex justify-center items-center h-full p-10">
+        <div className="bg-cyan-500 aspect-[1/1.414] h-full"></div>
+      </div>
+    </div>
+  );
+}
+
+function DateForm() {
+  return (
+    <div className="w-full border-2 max-h-50 border-foreground rounded-sm">
+      <div className="flex justify-between items-center bg-foreground font-light h-[4vh] w-full text-[1.2rem] px-2">
+        <p>Dates</p>
+        <IoIosArrowDropdown className="text-[1.7rem]" />
+      </div>
+    </div>
+  );
+}
+
+function FromForm() {
+  return (
+    <div className="w-full border-2 max-h-50 border-foreground rounded-sm">
+      <div className="flex justify-between items-center bg-foreground font-light h-[4vh] w-full text-[1.2rem] px-2">
+        <p>From</p>
+        <IoIosArrowDropdown className="text-[1.7rem]" />
+      </div>
+    </div>
+  );
+}
+
+function ToForm() {
+  return (
+    <div className="w-full border-2 max-h-50 border-foreground rounded-sm">
+      <div className="flex justify-between items-center bg-foreground font-light h-[4vh] w-full text-[1.2rem] px-2">
+        <p>To</p>
+        <IoIosArrowDropdown className="text-[1.7rem]" />
+      </div>
+    </div>
+  );
+}
+
+function MiscForm() {
+  return (
+    <div className="w-full border-2 max-h-50 border-foreground rounded-sm">
+      <div className="flex justify-between items-center bg-foreground font-light h-[4vh] w-full text-[1.2rem] px-2">
+        <p>Miscellaneous</p>
+        <IoIosArrowDropdown className="text-[1.7rem]" />
+      </div>
     </div>
   );
 }
--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <div className="w-full flex justify-between overflow-hidden">
+        <div className="w-full flex justify-between overflow-hidden select-none">
             <h1 className="font-koulen text-foreground tracking-[-5%] leading-[0.8] text-[clamp(3rem,20vw,6rem)]">
                 Pinvoice
             </h1>
