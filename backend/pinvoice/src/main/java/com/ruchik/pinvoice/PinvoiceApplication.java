8e9bbfbddbc86d4d7fc291f4d32bea30a05f233e
ruchik-panwal <ruchik.panwal@gmail.com>
2026-06-30 15:50:33 +0000

    back

---
+++ backend/pinvoice/src/main/java/com/ruchik/pinvoice/PinvoiceApplication.java
@@ -0,0 +1,13 @@
+package com.ruchik.pinvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinvoiceApplication.class, args);
+	}
+
+}
---
+++ backend/pinvoice/src/test/java/com/ruchik/pinvoice/PinvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruchik.pinvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
